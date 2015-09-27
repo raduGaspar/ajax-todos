a41428958fda6f1d00dc60a140954ce554dd0cf8
Radu Gaspar <radu.bogdan.gaspar@gmail.com>
2015-09-27 14:20:05 +0300

    boilerplate setup

---
+++ README.md
@@ -0,0 +1,18 @@
+# AJAX TODOs app
+
+### Boilerplate for the AJAX workshop
+
+Based on the
+[TODOs API](https://todos-radugaspar.c9.io/)
+using JavaScript with `XMLHttpRequest()`
+
+### Fastest way to run this project
+- [Download NodeJS](https://nodejs.org/)
+- clone this repo
+- open a terminal inside the cloned repo
+
+  ```
+  npm install http-server -g
+  http-server
+  ```
+- project should now be running on `http://localhost:8080/`
---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+(function() {
+
+  var root = 'https://todos-radugaspar.c9.io',
+    lists = root + '/lists',
+    client = new XMLHttpRequest(),
+    processData = function(data) {
+      console.log(data, JSON.parse(data.response));
+    },
+    handler = function() {
+      if(this.readyState == this.DONE) {
+        if(this.status == 200) {
+          processData(this);
+          return;
+        }
+        // something went wrong
+        processData(null);
+      }
+    };
+
+  client.onreadystatechange = handler;
+  client.open("GET", lists);
+  client.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  client.send();
+  // client.open("POST", lists);
+  // client.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  // client.send(JSON.stringify({name: 'Food'}));
+}());
+
+// basic helper function
+function ajax(type, url, data, cb) {
+  var xhr = new XMLHttpRequest(),
+    handler = function() {
+      if(this.readyState == this.DONE) {
+        if(this.status == 200) {
+          cb(this.response || this.responseText);
+          return;
+        }
+        // something went wrong
+        cb(null);
+      }
+    };
+
+  xhr.onreadystatechange = handler;
+  xhr.open(type, url);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.send(data);
+
+  return xhr; // not a bad idea to also return the request object
+}
